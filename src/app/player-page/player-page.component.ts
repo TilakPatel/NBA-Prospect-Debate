e7eae12e9578926a0d5cf37ca781be8a00bd49fc
Tilak Patel <ptilak142@gmail.com>
2019-01-10 17:48:17 -0500

    playerPage updates

--- src/app/player-page/player-page.component.ts
+++ src/app/player-page/player-page.component.ts
@@ -22,19 +22,19 @@ export class PlayerPageComponent implements OnInit {
   ngOnInit() {
     $('[data-toggle="tooltip"]').tooltip();
     this.route.params.subscribe(params => this.playerName = params['name']);
-    this.http.post<Player>(this.serverURL+'/getPlayer',
+    this.http.post<Player>(this.serverURL + '/getPlayer',
       {
         name: this.playerName
       }
     ).subscribe(dat => {
       this.player = dat;
       this.player.analysises.sort((a, b) => b.popularity - a.popularity);
-      if(this.player.analysises && this.player.analysises.length){ //not empty
-        this.analysisText = this.player.analysises[0].analysis;
-        this.analysisContributor = this.player.analysises[0].contributor;
-      } else {
+      if (this.player.analysises[0].analysis = 'vote for the other one') { //empty
         this.analysisText = "No analysis has been written.";
         this.analysisContributor = "No analysis has been written.";
+      } else {
+        this.analysisText = this.player.analysises[0].analysis;
+        this.analysisContributor = this.player.analysises[0].contributor;
       }
     },
       err => { console.log(err) }
@@ -64,23 +64,23 @@ export class PlayerPageComponent implements OnInit {
       ageSubtract = 2000 - parseInt(player.year, 10);
     }
     let att = player.attributes;
-    return (100 * (this.getAvg(att.athleticism) * (2.5/115) + this.getAvg(att.defense) * (1/115) + this.getAvg(att.dribbling) * (1/115) + this.getAvg(att.intangibles) * (0.5/115) + this.getAvg(att.leadership) * (0.5/115) +
-      this.getAvg(att.nba_ready) * (1/115) + this.getAvg(att.passing) * (1/115) + this.getAvg(att.potential) * (2.5/115) + this.getAvg(att.shooting) * (2/115) + this.getAvg(att.size) * (1/115) - this.getAvg(att.projectedDurability) * (0.5/115)) + ageSubtract + 5).toFixed(2);
+    return (100 * (this.getAvg(att.athleticism) * (2.5 / 115) + this.getAvg(att.defense) * (1 / 115) + this.getAvg(att.dribbling) * (1 / 115) + this.getAvg(att.intangibles) * (0.5 / 115) + this.getAvg(att.leadership) * (0.5 / 115) +
+      this.getAvg(att.nba_ready) * (1 / 115) + this.getAvg(att.passing) * (1 / 115) + this.getAvg(att.potential) * (2.5 / 115) + this.getAvg(att.shooting) * (2 / 115) + this.getAvg(att.size) * (1 / 115) - this.getAvg(att.projectedDurability) * (0.5 / 115)) + ageSubtract + 5).toFixed(2);
 
   }
   capitalizeWord(word: String) {
     if (word.indexOf('-') == -1) {
       return word.charAt(0).toUpperCase() + word.slice(1);
     } else {
-      return word.charAt(0).toUpperCase() + word.slice(1,word.indexOf('-') + 1) + word.charAt(word.indexOf('-') + 1).toUpperCase() + word.slice(word.indexOf('-') + 2);
+      return word.charAt(0).toUpperCase() + word.slice(1, word.indexOf('-') + 1) + word.charAt(word.indexOf('-') + 1).toUpperCase() + word.slice(word.indexOf('-') + 2);
     }
 
   }
 
   capitalizeName(name: String) {
     let arr = name.split(' ');
-    let rName : String = '';
-    for(let s in arr){
+    let rName: String = '';
+    for (let s in arr) {
       rName += this.capitalizeWord(arr[s]) + ' ';
     }
     return rName;
